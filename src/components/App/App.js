c6e46ba940f2f4d50f35e9a48f6471d31f56f48f
Christopher Black <chris@primeacademy.io>
2018-08-30 15:12:59 -0500

    Updated assignment to remove axios.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,26 +1,8 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import './App.css';
-import StudentForm from '../StudentForm/StudentForm';
 
 class App extends Component {
-  constructor() {
-    super();
-    // Keep track of the student list
-    this.state = {
-      studentList: [],
-    };
-
-    // Give our function access to `this`
-    this.addStudent = this.addStudent.bind(this);
-  }
-
   // This function is called by the StudentForm when the submit button is pressed
-  addStudent(newStudent) {
-    console.log(newStudent);
-    // POST your data here
-  }
-
   render() {
     return (
       <div className="App">
@@ -28,8 +10,8 @@ class App extends Component {
           <h1 className="App-title">GitHub Student List</h1>
         </header>
         <br/>
-        <StudentForm addStudent={this.addStudent}/>
-
+        <p>Student form component goes here.</p>
+        <hr/>
         <p>Student list goes here.</p>
       </div>
     );
