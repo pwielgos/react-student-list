4f6df88f8a63e39defe39947b4c7586345107499
Chad Dornfeld <cmdornfeld@gmail.com>
2019-12-06 09:57:17 -0600

    set up database and added test data to database.sql

--- database.sql
+++ database.sql
@@ -5,3 +5,8 @@ CREATE TABLE students (
 	id SERIAL PRIMARY KEY,
 	github_name varchar(255)
 );
+
+INSERT INTO students ("github_name") VALUES ('cmdornfeld');
+INSERT INTO students ("github_name") VALUES ('BenStretar');
+INSERT INTO students ("github_name") VALUES ('elmiabdirahman');
+INSERT INTO students ("github_name") VALUES ('pwielgos');
